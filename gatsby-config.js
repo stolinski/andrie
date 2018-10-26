e4caf22b3b8c633f5531d9c8b2a148530c325252
Scott <scott.tolinski@gmail.com>
2018-10-25 21:19:07 -0600

    removes wp config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,17 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Andrie',
   },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
-    {
-      resolve: 'gatsby-source-wordpress',
-      options: {
-        baseUrl: 'thewholemind.me',
-        protocol: 'http',
-        hostingWPCOM: true,
-        useACF: true,
-      },
-    },
-  ],
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
 }
