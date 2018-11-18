0bd49cc44e2885f9b891e81d85bc62aa60efb0a1
Scott <scott.tolinski@gmail.com>
2018-11-18 11:42:10 -0700

    adds equipment detail page

--- src/pages/equipment/barbara.js
+++ src/pages/equipment/barbara.js
@@ -1,26 +1,97 @@
 import React from 'react'
+import {Link } from 'gatsby'
 import styled from 'styled-components'
 import Layout from '../../components/Layout'
-import { Zone, BoxWrapper, Box, Button, Grid } from '../../elements'
+import { Zone, BoxWrapper, Box, Button, Grid, blue, above } from '../../elements'
+import barbara from '../../images/barbara-full.jpg'
 
 const EquipmentPage = () => (
   <Layout>
-    <div>
-      <Zone modifiers={['solid']} hero={true}>
         <EquipHeading>
-          <h3>Tugboats</h3>
+          <Link to="/equipment">&laquo; Back to main list</Link>
         </EquipHeading>
-      </Zone>
-    </div>
+        <EquipBody bg={barbara}>
+          <EquipDetails>
+            <h1>Barbara Andrie</h1>
+            <p>2000 HP Tugboat</p>
+<ul>
+<li>
+  <span>Length</span> 122'
+  </li>
+  <li>
+  <span>Width</span> 29.6'
+  </li>
+  <li>
+  <span>Power</span> 2,000'
+  </li>
+  <li>
+  <span>Capacity</span> 25,000 gallons
+  </li>
+</ul>
+<h3>For more information, 
+please contact:</h3>
+<ul>
+<li>Name </li>
+<li>Phone </li>
+<li>Email </li>
+<li>Request </li>
+</ul>
+          </EquipDetails>
+        </EquipBody>
   </Layout>
 )
 
 export default EquipmentPage
 
 const EquipHeading = styled.div`
-  h3 {
-    font-size: 2rem;
+  padding: 2rem 5%;
+  padding-top: 180px;
+  margin-top: -230px;
+  ${above.med`
+    margin-top: -130px;
+  `};
+  a {
+    color: white;
     text-transform: uppercase;
   }
-  margin-bottom: 2rem;
+  background: ${blue};
+`
+
+const EquipBody = styled.div`
+  height: 70vh;
+  background: url(${({bg}) => bg});
+  background-size: cover;
+  padding: 2rem 5%;
+`
+
+const EquipDetails = styled.div`
+  background: rgba(0,0,0,0.7);
+  width: 400px;
+  margin-left: auto;
+  color: white;
+  padding: 40px;
+  h1 {
+    font-size: 24px;
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  li {
+    margin-bottom: 4px;
+    span {
+      font-weight: bold;
+      text-transform: uppercase;
+    }
+  }
+  p {
+    margin-top: 0;
+  }
+  h3 {
+    margin: 2rem 0 10px;
+    text-transform: uppercase;
+    font-size: 16px;
+  }
 `
--- src/pages/safety.js
+++ src/pages/safety.js
@@ -8,36 +8,25 @@ const IndexPage = () => (
   <Layout>
     <div>
       <Zone modifiers={['right', 'dark']} hero={true} image={hero}>
-        <div className="zone-content">
-          <h2>Putting our people and the environment first</h2>
-          <p>
-            From the Great Lakes to the Illinois and Mississippi river systems,
-            Andrie is the Midwest’s premiere marine transportation and service
-            company. Founded in 1988, we provide a wide range of award-winning
-            services to a broad range of customers – from local, national, and
-            global corporations to small and medium-sized companies alike.
-          </p>
-        </div>
-        <div className="zone-content zone-content--secondary">
-          <h2>Safety First</h2>
-          <p>
-            From the Great Lakes to the Illinois and Mississippi river systems,
-            Andrie is the Midwest’s premiere marine transportation and service
-            company. Founded in 1988, we provide a wide range of award-winning
-            services to a broad range of customers – from local, national, and
-            global corporations to small and medium-sized companies alike.
-          </p>
+      <div className="zone-content">
+        <h2>Safety First</h2>
+        <p>We strongly believe that safety strengthens our organization and improves the quality of life for our employees and customers. Safety is our #1 priority. We want everyone to go home safe at the end of each day.</p>
+        <p>As a member of the A team, we expect everyone to contribute to improve our safety culture. We strive to affirmatively abide by these 5 statements in order to help us strengthen our safety culture.</p>
+        <ol>
+          <li>I must assess and control risks before starting any task.</li>
+          <li>I must only perform activities for which I am authorized.</li>
+          <li>I must never override or misuse health and safety devices.</li>
+          <li>I must always use the required PPE.</li>
+          <li>I must never work under the influence of alcohol or drugs</li>
+        </ol>
         </div>
       </Zone>
       <Zone modifiers={['solid', 'center', 'short']}>
-        <h3>Our Promise</h3>
+        <h3>First Choice</h3>
         <p style={{ fontSize: '24px' }}>
-          Andrie strives for efficiency and effectiveness. Our empowered
-          employee teams strive to be the benchmark in safety and performance
-          across the markets we serve. We value each other, we value our
-          dedicated vendors, we value our lasting customer relationships.
-        </p>
-        <Button modifiers={['dark']}>View All Equipment</Button>
+        Our mission at Andrie is to empower our employees to server in a safe, cost effective and environmentally sound manner. In order to achieve this we have made the promise to be the company of First Choice.</p>
+        <p style={{ fontSize: '24px' }}>
+        Andrie strives to be the company of First Choice for our employees, customers and our unique ecosystem that we operate within. We believe in achieving this we all leave the world in a better place for our future generations.</p> 
       </Zone>
     </div>
   </Layout>
